bb3ba31f73f063b40d9bf12c1c7bacee9798ff49
Liza George <liza@chiramattel.com>
2025-01-16 11:42:52 -0800

    Handle errors in script

--- generate-portfolio.sh
+++ generate-portfolio.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # generate-portfolio.sh
+set -e
+
+error_handler() {
+    echo "Generate Portfolio: Error occurred in command: $1"
+    exit 1
+}
 
 # Check if Node.js is installed
 if ! command -v node >/dev/null 2>&1; then
@@ -17,7 +23,6 @@ if ! command -v pnpm >/dev/null 2>&1; then
 fi
 
 
-
 # Install required dependencies
 echo "> Installing required dependencies..."
 pnpm --silent i
@@ -27,13 +32,13 @@ node generate-portfolio.js
 
 cd ../my-portfolio # TODO: temporary, till we use the user-defined dest path
 
-git init -q
-git add .
-git commit -qm "Initial commit"
+git init -q || error_handler "Failed to initialize git repository"
+git add . || error_handler "Failed to stage files"
+git commit -qm "Initial commit" || error_handler "Failed to create initial commit"
 
-gh repo create my-portfolio --public --source .
+gh repo create my-portfolio --public --source .  || error_handler "Failed to create GitHub repository"
 git branch -M main -q
-git push -uq origin main
+git push -uq origin main || error_handler "Failed to push to remote"
 
 echo "Your portfolio is now available in your GitHub profile."
 
